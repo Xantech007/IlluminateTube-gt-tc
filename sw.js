bd2170151e7451b3c0344382f7fef73010331818
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-09-01 13:52:27 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple fetch pass-through required for basic PWA installation compliance
+  event.respondWith(fetch(event.request));
+});
