46fde9ca7ac3217fd0bf08a79cdb679ab7870a59
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-09-04 09:18:33 +0100

    Update profile.php

--- users/profile.php
+++ users/profile.php
@@ -132,7 +132,7 @@ $error_message = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : null
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>Profile | Illuminate Tube</title>
+    <title>Withdraw | Illuminate Tube</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
